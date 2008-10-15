5c1356ddb1bc89d9a481757367383ce1d9e1933b
Anthony Thibault <ajt@hyperlogic.org>
2008-10-15 14:52:19 -0700

    re-arranged driver a bit for easier debugging of disassembly.

--- mathtest.cpp
+++ mathtest.cpp
@@ -2,22 +2,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+float u[3];
+float v[3];
+Vector3 x;
+Vector3 y;
+Vector3 r;
+
 int main(int argc, char* argv[])
 {
-	float u[3];
 	u[0] = atof(argv[1]);
 	u[1] = atof(argv[2]);
 	u[2] = atof(argv[3]);
 
-	float v[3];
 	v[0] = atof(argv[4]);
 	v[1] = atof(argv[5]);
 	v[2] = atof(argv[6]);
 
-	Vector3 x(u[0], u[1], u[2]);
-	Vector3 y(v[0], v[1], v[2]);
+	x.Set(u[0], u[1], u[2]);
+	y.Set(v[0], v[1], v[2]);
 
-	Vector3 r = x + y;
+	r = x + y;
 
 	printf("r = %.5f, %.5f, %.5f\n", r.x, r.y, r.z);
 
