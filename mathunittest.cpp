e939b36da6d5b049c7612a5bc269890e724286ee
Anthony Thibault <ajt@hyperlogic.org>
2008-10-17 13:12:30 -0700

    added subtraction test, refactored tests a bit using templates.

--- mathunittest.cpp
+++ mathunittest.cpp
@@ -14,8 +14,20 @@ std::vector<Vector3> s_vector3Vec;
 std::vector<Vector4> s_vector4Vec;
 std::vector<Matrix> s_matrixVec;
 
-float s_floatResult;
-Vector4 s_vector4Result;
+bool FuzzyEqual(float rhs, float lhs)
+{
+	const float epsilon = 0.0001f;
+	return fabs(rhs - lhs) <= epsilon;
+}
+
+bool FloatTest(float rhs, float lhs)
+{
+	// For testing purposes, two Nans are "equal" even if they have different bits.
+	if (isnan(rhs) && isnan(lhs))
+		return true;
+
+	return rhs == lhs;
+}
 
 // 0..1
 float RandomNormalizedFloat()
@@ -29,7 +41,7 @@ float RandomFloat(float range = 10000.0f)
 	return (((float)rand() / (RAND_MAX / 2)) - 1.0f) * range;
 }
 
-void InitData()
+void InitTestData()
 {
 	srand((unsigned int)mach_absolute_time());
 
@@ -115,65 +127,68 @@ void InitData()
 	// TODO: nans, q-nans & denormalized...
 }
 
-class Vector2AddTest : public TestCase
+class Vector2Addition
 {
 public:
-	Vector2AddTest() : TestCase("Add") {}
-	~Vector2AddTest() {}
-
-	bool Test() const;
+	static const char* GetName() { return "Addition"; }
+	bool operator()(const Vector2& a, const Vector2& b) const
+	{
+		float ax = a.X(); float ay = a.Y();
+		float bx = b.X(); float by = b.Y();
+		float rx = ax + bx;	float ry = ay + by;
+		Vector2 r = a + b;
+		return FloatTest(r.X(), rx) && FloatTest(r.Y(), ry);
+	}
 };
 
-bool FuzzyEqual(float rhs, float lhs)
+class Vector2Subtraction
 {
-	const float epsilon = 0.0001f;
-	return fabs(rhs - lhs) <= epsilon;
-}
-
-bool ftest(float rhs, float lhs)
-{
-	// For testing purposes, two Nans are "equal" even if they have different bits.
-	if (isnan(rhs) && isnan(lhs))
-		return true;
-
-	return rhs == lhs;
-}
-
-bool Vector2AddTest::Test() const
-{
-	for (unsigned int i = 0; i < s_vector2Vec.size(); ++i)
+public:
+	static const char* GetName() { return "Subtraction"; }
+	bool operator()(const Vector2& a, const Vector2& b) const
 	{
-		for (unsigned int j = 0; j < s_vector2Vec.size(); ++j)
+		float ax = a.X(); float ay = a.Y();
+		float bx = b.X(); float by = b.Y();
+		float rx = ax - bx; float ry = ay - by;
+		Vector2 r = a - b;
+		return FloatTest(r.X(), rx) && FloatTest(r.Y(), ry);
+	}
+};
+
+template <class BinaryOp>
+class Vector2BinaryOpTest : public TestCase
+{
+public:
+	Vector2BinaryOpTest() : TestCase(BinaryOp::GetName()) {}
+	~Vector2BinaryOpTest() {}
+
+	bool Test() const
+	{
+		BinaryOp op;
+		for (unsigned int i = 0; i < s_vector2Vec.size(); ++i)
 		{
-			Vector2 a = s_vector2Vec[i];
-			Vector2 b = s_vector2Vec[j];
-
-			float ax = a.X();
-			float ay = a.Y();
-			float bx = b.X();
-			float by = b.Y();
-			float rx = ax + bx;
-			float ry = ay + by;
-
-			Vector2 r = a + b;
-
-			if (!ftest(r.X(), rx) || !ftest(r.Y(), ry))
+			for (unsigned int j = 0; j < s_vector2Vec.size(); ++j)
 			{
-				printf("a = (%.5f, %.5f), b = (%.5f, %.5f)\n", a.X(), a.Y(), b.X(), b.Y());
-				printf("r = (%.5f, %.5f), rx = %.5f, ry = %.5f\n", r.X(), r.Y(), rx, ry);
-				return false;
+				Vector2 a = s_vector2Vec[i];
+				Vector2 b = s_vector2Vec[j];
+				if (!op(a, b))
+				{
+					printf("a = (%.5f, %.5f), b = (%.5f, %.5f)\n", a.X(), a.Y(), b.X(), b.Y());
+					return false;
+				}
 			}
 		}
+		return true;
 	}
-	return true;
-}
+};
 
 int main(int argc, char* argv[])
 {
-	InitData();
+	InitTestData();
 
 	TestSuite vector2Suite("Vector2");
-	vector2Suite.AddTest(new Vector2AddTest());
+	vector2Suite.AddTest(new Vector2BinaryOpTest<Vector2Addition>());
+	vector2Suite.AddTest(new Vector2BinaryOpTest<Vector2Subtraction>());
 	vector2Suite.RunTests();
 
 	return 0;
