27413e5b096c9e0a134b539fe52a3b79390ab1ee
Anthony Thibault <ajt@hyperlogic.org>
2010-09-27 17:31:27 -0700

    More lua ext work.

--- lua/abaci_ext.cpp
+++ lua/abaci_ext.cpp
@@ -8,6 +8,89 @@ extern "C" {
 #include "lauxlib.h"
 }
 
+static int deg_to_rad(lua_State* L)
+{
+	double d = luaL_checknumber(L, 1);
+	lua_pushnumber(L, DegToRad(d));
+	return 1;
+}
+
+static int rad_to_deg(lua_State* L)
+{
+	double r = luaL_checknumber(L, 1);
+	lua_pushnumber(L, RadToDeg(r));
+	return 1;
+}
+
+static int clamp(lua_State* L)
+{
+	double v = luaL_checknumber(L, 1);
+	double min = luaL_checknumber(L, 2);
+	double max = luaL_checknumber(L, 3);
+	lua_pushnumber(L, Clamp(v, min, max));
+	return 1;
+}
+
+static int limit_pi(lua_State* L)
+{
+	double t = luaL_checknumber(L, 1);
+	lua_pushnumber(L, LimitPi(t));
+	return 1;
+}
+
+static int mod_two_pi(lua_State* L)
+{
+	double t = luaL_checknumber(L, 1);
+	lua_pushnumber(L, Mod2Pi(t));
+	return 1;
+}
+
+static int fuzzy_equals(lua_State* L)
+{
+	double a = luaL_checknumber(L, 1);
+	double b = luaL_checknumber(L, 1);
+	lua_pushboolean(L, FuzzyEqual(a, b));
+	return 1;
+}
+
+static int lerp(lua_State* L)
+{
+	double a = luaL_checknumber(L, 1);
+	double b = luaL_checknumber(L, 2);
+	double t = luaL_checknumber(L, 3);
+	lua_pushnumber(L, Lerp(a, b, t));
+	return 1;
+}
+
+static int rand_int(lua_State* L)
+{
+	int min = luaL_checkinteger(L, 1);
+	int max = luaL_checkinteger(L, 2);
+	lua_pushinteger(L, RandomInt(min, max));
+	return 1;
+}
+
+static int rand(lua_State* L)
+{
+	double min = luaL_checknumber(L, 1);
+	double max = luaL_checknumber(L, 2);
+	lua_pushnumber(L, RandomScalar(min, max));
+	return 1;
+}
+
+static const luaL_Reg abaci_lib_f [] = {
+	{"deg_to_rad", deg_to_rad},
+	{"rad_to_deg", rad_to_deg},
+	{"clamp", clamp},
+	{"limit_pi", limit_pi},
+	{"mod_two_pi", mod_two_pi},
+	{"fuzzy_equals", fuzzy_equals},
+	{"lerp", lerp},
+	{"rand_int", rand_int},
+	{"rand", rand},
+	{NULL, NULL}
+};
+
 #define check_vec2(L) (Vector2f*)luaL_checkudata(L, 1, "abaci.vec2")
 
 static int vec2_new(lua_State* L)
@@ -58,6 +141,9 @@ static const luaL_Reg vec2_lib_m [] = {
 
 int luaopen_abaci(lua_State* L)
 {
+	luaL_newmetatable(L, "abaci.abaci");
+	luaL_register(L, "abaci", abaci_lib_f);
+
 	luaL_newmetatable(L, "abaci.vec2");
 
 	lua_pushvalue(L, -1);
@@ -65,5 +151,6 @@ int luaopen_abaci(lua_State* L)
 	luaL_register(L, NULL, vec2_lib_m);
 	
 	luaL_register(L, "vec2", vec2_lib_f);
+
 	return 1;
 }
