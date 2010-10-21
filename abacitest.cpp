7a4ed95aa24a1f4ab85bb002bbcd13f0667e4d18
Anthony Thibault <ajt@hyperlogic.org>
2010-10-21 14:12:58 -0700

    Removed Licence notice from source files into LICENCE file. Unit tests are less verbose, so it's easier to catch failures.

--- abacitest.cpp
+++ abacitest.cpp
@@ -1,27 +1,3 @@
-/*
-The MIT License
-
-Copyright (c) 2008 Anthony J. Thibault
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
-
 #include "abaci.h"
 #include <vector>
 #include <stdio.h>
@@ -568,6 +544,21 @@ public:
 		return FloatTest(rx, r.x) && FloatTest(ry, r.y);
 	}
 };
+
+class Vector2fSetZeroTest : public TestCase
+{
+public:
+	Vector2fSetZeroTest() : TestCase("SetZero") {}
+	~Vector2fSetZeroTest() {}
+
+	bool Test() const
+	{
+		Vector2f v(1,1);
+		v.SetZero();
+		return (v.x == 0.0f) && (v.y == 0.0f);
+	}
+};
+
 
 //
 // Vector3f tests
@@ -1856,6 +1847,7 @@ int main(int argc, char* argv[])
 	vector2Suite.AddTest(new Vector2fBinaryOpTest<Vector2fDotProduct>());
 	vector2Suite.AddTest(new Vector2fBinaryOpTest<Vector2fCompMul>());
 	vector2Suite.AddTest(new Vector2fBinaryOpTest<Vector2fCompDiv>());
+	vector2Suite.AddTest(new Vector2fSetZeroTest());
 	vector2Suite.RunTests();
 	// TODO: SetZero()
 	// TODO: Set()
--- unittest.cpp
+++ unittest.cpp
@@ -1,27 +1,3 @@
-/*
-The MIT License
-
-Copyright (c) 2008 Anthony J. Thibault
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
-
 #include "unittest.h"
 #include <stdio.h>
 
@@ -42,14 +18,14 @@ bool TestSuite::RunTests() const
 	bool suiteResult = true;
 	for(TestCaseVec::const_iterator iter = m_testCaseVec.begin(); iter != m_testCaseVec.end(); ++iter)
 	{
-		printf("\tTest : %s : ... ", (*iter)->GetDescription().c_str());
 		bool result = (*iter)->Test();
 		suiteResult &= result;
 
-		if (result)
-			printf(" Success\n");
-		else
+		if (!result)
+		{
+			printf("\tTest : %s : ... ", (*iter)->GetDescription().c_str());
 			printf(" Failure\n");
+		}
 	}
 	
 	printf("Test Suite %s Result : %s\n", m_description.c_str(), suiteResult ? "Success" : "Failure");
