577ae58194cfa8030c1fd20ff3fcca786facc06e
Anthony Thibault <ajt@hyperlogic.org>
2010-12-02 20:40:53 -0800

    Lua extentions for vec2.

--- lua/abaci_ext.cpp
+++ lua/abaci_ext.cpp
@@ -1,12 +1,18 @@
 // lua extentions
 
 #include "abaci.h"
+#include <stdio.h>
+#include <string.h>
 
 extern "C" {
 #include "lua.h"
 #include "lualib.h"
 #include "lauxlib.h"
 }
+
+//
+// abaci functions
+//
 
 static int deg_to_rad(lua_State* L)
 {
@@ -48,7 +54,7 @@ static int mod_two_pi(lua_State* L)
 static int fuzzy_equals(lua_State* L)
 {
 	double a = luaL_checknumber(L, 1);
-	double b = luaL_checknumber(L, 1);
+	double b = luaL_checknumber(L, 2);
 	lua_pushboolean(L, FuzzyEqual(a, b));
 	return 1;
 }
@@ -91,67 +97,312 @@ static const luaL_Reg abaci_lib_f [] = {
 	{NULL, NULL}
 };
 
-#define check_vec2(L) (Vector2f*)luaL_checkudata(L, 1, "abaci.vec2")
+//
+// vec2 functions
+//
+
+#define check_vec2(L, n) (Vector2f*)luaL_checkudata(L, n, "abaci.vec2")
+#define new_vec2(L, name)                                             \
+	Vector2f* name = (Vector2f*)lua_newuserdata(L, sizeof(Vector2f)); \
+	luaL_getmetatable(L, "abaci.vec2");                               \
+	lua_setmetatable(L, -2)
 
 static int vec2_new(lua_State* L)
 {
-	double x = luaL_checknumber(L, 1);
-	double y = luaL_checknumber(L, 2);
-	Vector2f* v = (Vector2f*)lua_newuserdata(L, sizeof(Vector2f));
-	v->Set((float)x, (float)y);
-
-	luaL_getmetatable(L, "abaci.vec2");
-	lua_setmetatable(L, -2);
-
+	lua_Number x = luaL_checknumber(L, 1);
+	lua_Number y = luaL_checknumber(L, 2);
+    new_vec2(L, result);
+	result->Set((float)x, (float)y);
 	return 1;
 }
+
+static int vec2_random_unit(lua_State* L)
+{
+    new_vec2(L, result);
+    *result = Vector2f::RandomUnitVector();
+    return 1;
+}
+
+static int vec2_lerp(lua_State* L)
+{
+    Vector2f* a = check_vec2(L, 1);
+    Vector2f* b = check_vec2(L, 2);
+    lua_Number t = luaL_checknumber(L, 3);
+
+    new_vec2(L, result);
+    *result = Lerp(*a, *b, (float)t);
+    return 1;
+}
+
+static const luaL_Reg vec2_class_funcs [] = {
+	{"new", vec2_new},
+    {"random_unit", vec2_random_unit},
+    {"lerp", vec2_lerp},
+	{NULL, NULL}
+};
+
+//
+// vec2 methods
+//
 
 static int vec2_len(lua_State* L)
 {
-	Vector2f* v = check_vec2(L);
-	lua_pushnumber(L, (double)v->Len());
-	return 1;
+    Vector2f* self = check_vec2(L, 1);
+    lua_pushnumber(L, self->Len());
+    return 1;
 }
 
-static int vec2_getx(lua_State* L)
+static int vec2_unit(lua_State* L)
 {
-	Vector2f* v = check_vec2(L);
-	lua_pushnumber(L, (double)v->x);
-	return 1;
+    Vector2f* self = check_vec2(L, 1);
+    new_vec2(L, result);
+    *result = self->Unit();
+    return 1;
 }
 
-static int vec2_gety(lua_State* L)
+static int vec2_min_len(lua_State* L)
 {
-	Vector2f* v = check_vec2(L);
-	lua_pushnumber(L, (double)v->y);
-	return 1;
+    Vector2f* self = check_vec2(L, 1);
+    lua_Number len = luaL_checknumber(L, 2);
+
+    new_vec2(L, result);
+    *result = self->MinLen(len);
+    return 1;
 }
 
-static const luaL_Reg vec2_lib_f [] = {
-	{"new", vec2_new},
+static const luaL_Reg vec2_method_funcs [] = {
+    {"len", vec2_len},
+    {"unit", vec2_unit},
+	{"min_len", vec2_min_len},
 	{NULL, NULL}
 };
 
-static const luaL_Reg vec2_lib_m [] = {
-	{"len", vec2_len},
-	{"getx", vec2_getx},
-	{"gety", vec2_gety},
+//
+// vec2 meta methods
+//
+
+static int vec2_index(lua_State* L)
+{
+    Vector2f* v = check_vec2(L, 1);
+    luaL_checkany(L, 2);
+
+    int key_type = lua_type(L, 2);
+    if (key_type == LUA_TNUMBER)
+    {
+        lua_Integer i = lua_tointeger(L, 2);
+        if (i >= 0 && i < 2)
+        {
+            lua_pushnumber(L, (*v)[i]);
+            return 1;
+        }
+
+        // error
+        lua_pushstring(L, "vec2: num index out of range, must be 0 or 1");
+        lua_error(L);
+    }
+    else if (key_type == LUA_TSTRING)
+    {
+        size_t size;
+        const char* s = lua_tolstring(L, 2, &size);
+        if (size == 1)
+        {
+            switch (s[0])
+            {
+            case 'x':
+                lua_pushnumber(L, v->x);
+                return 1;
+            case 'y':
+                lua_pushnumber(L, v->y);
+                return 1;
+            }
+        }
+        else
+        {
+            // search for string key in vec2_method_funcs array.
+            int i = 0;
+            while (vec2_method_funcs[i].name)
+            {
+                if (!strcmp(s, vec2_method_funcs[i].name))
+                {
+                    lua_pushcfunction(L, vec2_method_funcs[i].func);
+                    return 1;
+                }
+                i++;
+            }
+        }
+
+        // error
+        lua_pushstring(L, "vec2: unknown string key, must be x or y");
+        lua_error(L);
+    }
+    else
+    {
+        // error
+        lua_pushstring(L, "vec2: unsupported key");
+        lua_error(L);
+    }
+
+    return 0;
+}
+
+static int vec2_newindex(lua_State* L)
+{
+    Vector2f* v = check_vec2(L, 1);
+    luaL_checkany(L, 2);
+    lua_Number value = luaL_checknumber(L, 3);
+
+    int key_type = lua_type(L, 2);
+    if (key_type == LUA_TNUMBER)
+    {
+        lua_Integer i = lua_tointeger(L, 2);
+        if (i >= 0 && i < 2)
+        {
+            (*v)[i] = value;
+            lua_pushnumber(L, (*v)[i]);
+            return 0;
+        }
+
+        // error
+        lua_pushstring(L, "vec2: num index out of range, must be 0 or 1");
+        lua_error(L);
+    }
+    else if (key_type == LUA_TSTRING)
+    {
+        size_t size;
+        const char* s = lua_tolstring(L, 2, &size);
+        if (size == 1)
+        {
+            switch (s[0])
+            {
+            case 'x':
+                v->x = value;
+                return 0;
+            case 'y':
+                v->y = value;
+                return 0;
+            }
+        }
+
+        // error
+        lua_pushstring(L, "vec2: unknown string key, must be x or y");
+        lua_error(L);
+    }
+    else
+    {
+        // error
+        lua_pushstring(L, "vec2: unsupported key");
+        lua_error(L);
+    }
+
+    return 0;
+}
+
+static int vec2_tostring(lua_State* L)
+{
+    Vector2f* v = check_vec2(L, 1);
+    char temp[64];
+    snprintf(temp, 64, "vec2(%.5f, %.5f)", v->x, v->y);
+    lua_pushstring(L, temp);
+    return 1;
+}
+
+static int vec2_lenop(lua_State* L)
+{
+    lua_pushinteger(L, 2);
+    return 1;
+}
+
+#define BINARY_VEC_OP_FUNC(name, op)            \
+    static int vec2_##name(lua_State* L)        \
+    {                                           \
+        Vector2f* a = check_vec2(L, 1);         \
+        Vector2f* b = check_vec2(L, 2);         \
+        new_vec2(L, result);                    \
+        *result = *a op *b;                     \
+        return 1;                               \
+    }
+
+BINARY_VEC_OP_FUNC(add, +)
+BINARY_VEC_OP_FUNC(sub, -)
+BINARY_VEC_OP_FUNC(mul, *)
+BINARY_VEC_OP_FUNC(div, /)
+
+static int vec2_dot(lua_State* L)
+{
+    Vector2f* a = check_vec2(L, 1);
+    Vector2f* b = check_vec2(L, 2);
+    lua_pushnumber(L, Dot(*a, *b));
+    return 1;
+}
+
+static int vec2_unm(lua_State* L)
+{
+    Vector2f* a = check_vec2(L, 1);
+    new_vec2(L, result);
+    *result = -*a;
+    return 1;
+}
+
+static const luaL_Reg vec2_meta_funcs [] = {
+    {"__index", vec2_index},
+    {"__newindex", vec2_newindex},
+	{"__len", vec2_lenop},
+    {"__tostring", vec2_tostring},
+    {"__add", vec2_add},
+    {"__sub", vec2_sub},
+    {"__mul", vec2_mul},
+    {"__div", vec2_div},
+    {"__pow", vec2_dot},  // ^ as dot product
+    {"__unm", vec2_unm},  // unary minus
 	{NULL, NULL}
 };
+
+/* stack notes: (i always forget this)
+   -1 is top, -2 is just under that.
+   1 is bottom of stack, 2 is the one above that.
+
+   3 | c | -1
+   2 | b | -2
+   1 | a | -3
+     -----
+
+   Conundrums  (lean toward :)
+   * should length of a vector be v.len or v:len()
+   * should unit vector be v.unit or v:unit()
+   * should min_len be or v.min_len(10), v:min_len(10)
+
+*/
 
 extern "C" int luaopen_abaci(lua_State* L)
 {
-//	luaL_newmetatable(L, "abaci.abaci");
+    // register abaci module table, and push it.
 	luaL_register(L, "abaci", abaci_lib_f);
 
+    // registers all of vec2_class_funcs functions in the abaci.vec2 table.
+    lua_newtable(L);
+    luaL_register(L, NULL, vec2_class_funcs);
+    lua_setfield(L, -2, "vec2");
+
+    // metatable for use with vec2 userdata.
 	luaL_newmetatable(L, "abaci.vec2");
 
-	lua_pushvalue(L, -1);
+    /*
+    // vec2_mt.__index = vec2_index
+    lua_pushcfunction(L, vec2_index);
 	lua_setfield(L, -2, "__index");
-	luaL_register(L, NULL, vec2_lib_m);
 
-	
-	luaL_register(L, "vec2", vec2_lib_f);
+    // vec2_mt.__newindex = vec2_newindex
+    lua_pushcfunction(L, vec2_newindex);
+	lua_setfield(L, -2, "__newindex");
+
+    // vec2_mt.__tostring = vec2_tostring
+    lua_pushcfunction(L, vec2_tostring);
+	lua_setfield(L, -2, "__tostring");
+    */
+
+    // registers all vec2_meta_funcs functions in vec2_mt
+	luaL_register(L, NULL, vec2_meta_funcs);
+    lua_pop(L, 1);
 
 	return 1;
 }
--- lua/test.lua
+++ lua/test.lua
@@ -1,10 +1,123 @@
 require "abaci"
+require "math"
+local vec2 = abaci.vec2
+local eq = abaci.fuzzy_equals
 
-print("hello world")
+print("abaci test")
 
-pi = abaci.deg_to_rad(180)
-print(string.format("pi = %.5f", pi))
+-- test fuzzy_equals it's used by everything else below
+assert(eq(1.00001, 1.0))
+assert(not eq(1.001, 1.0))
 
+-- deg_to_rad
+assert(eq(math.pi, abaci.deg_to_rad(180)))
+
+-- rad_to_deg
+assert(eq(180, abaci.rad_to_deg(math.pi)))
+
+-- clamp
+min = 1.0
+max = 3.0
+assert(eq(2.0, abaci.clamp(2.0, min, max)))
+assert(eq(min, abaci.clamp(0.0, min, max)))
+assert(eq(max, abaci.clamp(9.0, min, max)))
+
+-- limit_pi
+delta = 0.1
+assert(eq(-math.pi + delta, abaci.limit_pi(math.pi + delta)))
+assert(eq(math.pi - delta, abaci.limit_pi(-math.pi - delta)))
+
+-- mod_two_pi
+assert(eq(delta, abaci.mod_two_pi(2 * math.pi + delta)))
+assert(eq(2 * math.pi - delta, abaci.mod_two_pi(-delta)))
+
+-- lerp
+assert(eq(10.0, abaci.lerp(0, 100, 0.1)))
+assert(eq(90.0, abaci.lerp(100, 0, 0.1)))
+
+-- rand_int
+min = 10
+max = 100
+for i=1,10000 do
+    r = abaci.rand_int(min, max)
+    assert(r >= min and r <= max)
+end
+
+-- rand
+min = 10
+max = 100
+for i=1,10000 do
+    r = abaci.rand(min, max)
+    assert(r >= min and r <= max)
+end
+
+-- new
 a = vec2.new(1, 2)
 
-print(string.format("a = (%.5f, %.5f), len = %.5f", a:getx(), a:gety(), a:len()));
+-- __index
+assert(a.x == 1)
+assert(a.y == 2)
+
+-- __newindex
+a.x = 0.1
+a.y = 0.2
+assert(eq(0.1, a.x))
+assert(eq(0.2, a.y))
+
+-- len
+b = vec2.new(0.3, 0.5)
+assert(eq(math.sqrt(b.x * b.x + b.y * b.y), b:len()))
+assert(eq(math.sqrt(a.x * a.x + a.y * a.y), a:len()))
+
+-- random_unit
+b = vec2.random_unit()
+assert(eq(1, math.sqrt(b.x * b.x + b.y * b.y)))
+
+-- lerp
+l = vec2.lerp(a, b, 0.3)
+assert(eq(abaci.lerp(a.x, b.x, 0.3), l.x))
+assert(eq(abaci.lerp(a.y, b.y, 0.3), l.y))
+
+-- min_len
+c = b:min_len(0.5)
+d = b:min_len(100.0)
+assert(eq(c:len(), 0.5))
+assert(eq(d:len(), b:len()))
+
+--print("a = ", a)
+--print("b = ", b)
+
+-- add
+sum = a + b
+assert(eq(sum.x, a.x + b.x))
+assert(eq(sum.y, a.y + b.y))
+
+-- sub
+dif = a - b
+assert(eq(dif.x, a.x - b.x))
+assert(eq(dif.y, a.y - b.y))
+
+-- mul
+prod = a * b
+assert(eq(prod.x, a.x * b.x))
+assert(eq(prod.y, a.y * b.y))
+
+-- div
+quo = a / b
+assert(eq(quo.x, a.x / b.x))
+assert(eq(quo.y, a.y / b.y))
+
+-- dot
+dot = a ^ b
+assert(eq(dot, a.x * b.x + a.y * b.y))
+
+-- unm
+neg = -b
+assert(eq(-b.x, neg.x))
+assert(eq(-b.y, neg.y))
+
+-- len
+assert(eq(#a, 2))
+
+
+print("Success!")
